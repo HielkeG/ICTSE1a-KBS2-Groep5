555b23deb06fba02945b9b4728373482c475e2ea
Carlo <carlodb@hotmail.com>
2017-11-15 14:51:33 +0100

    Begin v2

---
+++ VirtualPiano/VirtualPiano/View/MainForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace VirtualPiano
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formContent = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // formContent
+            // 
+            this.formContent.Location = new System.Drawing.Point(96, 124);
+            this.formContent.Name = "formContent";
+            this.formContent.Size = new System.Drawing.Size(420, 296);
+            this.formContent.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 564);
+            this.Controls.Add(this.formContent);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel formContent;
+    }
+}
+
---
+++ VirtualPiano/VirtualPiano/View/MainForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VirtualPiano.View;
+
+
+namespace VirtualPiano
+{
+    public partial class MainForm : Form
+        {
+            private ComposeView _composeView;
+        public MainForm()
+        {
+            InitializeComponent();
+            ShowComposeView();
+        }
+
+        public void ShowComposeView()
+        {
+            _composeView = new ComposeView()
+            {
+                Dock = DockStyle.Fill
+            };
+            formContent.Controls.Add(_composeView);
+        }
+
+            //public void StartOver()
+            //{
+            //    formContent.Controls.Remove(_mainView);
+            //    _mainView.Dispose();
+
+            //    ShowStartView();
+            //}
+
+    }
+}
