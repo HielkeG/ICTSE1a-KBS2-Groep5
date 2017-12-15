5d758125cb33cdedc1a6814b096a7b30aa6d7ed4
SanderHagen <sanderhagen@outlook.com>
2017-12-15 11:35:09 +0100

    geluid bugfix

--- VirtualPiano/VirtualPiano/Model/Song.cs
+++ VirtualPiano/VirtualPiano/Model/Song.cs
@@ -134,9 +134,17 @@ namespace VirtualPiano.Model
 
                                 //Noot afspelen
                                 string pitchTemp = note.Tone.ToString() + note.Octave.ToString();
-                                Enum.TryParse(pitchTemp, out Pitch pitch);
-                                MusicController.outputDevice.SendNoteOn(Channel.Channel1, pitch, 127);
-                                
+                                if (pitchTemp.Length == 4)
+                                {
+                                    Enum.TryParse(note.Tone.First().ToString() + "Sharp" + note.Octave.ToString(),out Pitch pitch);
+                                    MusicController.outputDevice.SendNoteOn(Channel.Channel1, pitch, 127);
+                                }
+                                else
+                                {
+                                    Enum.TryParse(pitchTemp, out Pitch pitch);
+                                    MusicController.outputDevice.SendNoteOn(Channel.Channel1, pitch, 127);
+                                }
+
                             }
                         }
                     }
